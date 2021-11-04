db490be0f2e4b213a646c6938f3b25241e4b1224
xuwangcheng <827910608@qq.com>
2021-11-04 09:03:19 +0800

    '首页商品详情开发1'

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -14,6 +14,8 @@
     <!--选项卡-->
     <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
 
+    <!--商品列表-->
+    <goods-list :goods="goods['pop'].list"/>
     <ul>
       <li>列表</li>
       <li>列表</li>
@@ -123,12 +125,13 @@
 <script>
   import NavBar from "components/common/navbar/NavBar"
   import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
 
   import HomeSwiper from "./childComponents/HomeSwiper"
   import HomeRecommendView from "./childComponents/HomeRecommendView";
   import HomeFeatureView from "./childComponents/HomeFeatureView"
 
-  import {getHomeMultidata} from "network/home"
+  import {getHomeMultidata,getHomeGoods} from "network/home"
   export default {
     name: "Home",
     components: {
@@ -137,6 +140,8 @@
       HomeSwiper,
       HomeRecommendView,
       TabControl,
+      getHomeGoods,
+      GoodsList
     },
     data(){
       return {
@@ -144,17 +149,42 @@
         recommends:[],
         goods:{
           'pop':{page:0,list:[]},
-          'news':{page: 0,list:[]},
+          'new':{page: 0,list:[]},
           'sell':{page:0,list:[]}
         }
       }
     },
     created() {
-      let t = this;
-      getHomeMultidata().then(res =>{
-        t.banners = res.data.banner.list
-        t.recommends = res.data.recommend.list
-      })
+      //请求多个数据
+      this.getHomeMultidata()
+
+      //请求商品数据 流行
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods:{
+      // 请求多个数据
+      getHomeMultidata(){
+        let t = this
+        getHomeMultidata().then(res =>{
+          t.banners = res.data.banner.list
+          t.recommends = res.data.recommend.list
+        })
+      },
+      // 请求商品数据
+      getHomeGoods(type){
+        let t = this
+        let page = t.goods[type].page + 1
+        getHomeGoods(type,page).then(res =>{
+          //取出列表
+          t.goods[type].list.push(...res.data.list)
+          //取出页码
+          t.goods[type].page += 1
+
+          console.log(t.goods)
+        })
+      }
     }
 
   }
@@ -180,5 +210,6 @@
   .tab-control{
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
